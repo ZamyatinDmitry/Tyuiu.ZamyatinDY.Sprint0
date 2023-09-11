409a468ae879b43c265f2e89d324e67cfde92513
Tyuiu.ZamyatinDY@outlook.com <dimazamatin18@gmail.com>
2023-09-11 16:24:12 +0500

    Task2

---
+++ Tyuiu.ZamyatinDY.Sprint0.Task2.V0.Test/DataTerviceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZamyatinDY.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ZamyatinDY.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataTerviceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Дмитрий";
+            var res = DataService.GetMessage(name);
+
+            // Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Дмитрий", res);
+        }
+    }
+}
---
+++ Tyuiu.ZamyatinDY.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZamyatinDY.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZamyatinDY.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a357c58-ec57-4257-a04b-d8f7ebb97292")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
